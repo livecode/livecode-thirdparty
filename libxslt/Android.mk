d6170285506390787a539e27474fe1cab16df45a
Mark Waddingham <mark.waddingham@runrev.com>
2013-09-11 15:17:46 +0100

    [[ RevXmlXslt ]] Adding missing files to Android makefile.

--- libxslt/Android.mk
+++ libxslt/Android.mk
@@ -8,9 +8,9 @@ LOCAL_MODULE := libxslt
 
 LOCAL_SRC_FILES := \
 	$(addprefix src/,attributes.c attrvt.c documents.c extensions.c \
-	functions.c imports.c keys.c namespaces.c \
+	functions.c imports.c keys.c namespaces.c security.c \
 	numbers.c pattern.c preproc.c templates.c transform.c \
-	variables.c xslt.c xsltlocale.c xsltutils.c)
+	variables.c xslt.c xsltlocale.c xsltutils.c extra.c)
 
 LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH)/include/libxslt \
